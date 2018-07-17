1265c788bea413a3570abc8088530b9595aefb99
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-17 10:24:52 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,15 @@
+package janani;
+
+public class Count {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count=0,n=12345;
+		while(n!=0){
+			n=n/10;
+			count ++;
+		}
+     System.out.println(count);
+	}
+
+}
